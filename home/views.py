b3b71eb4d78b8b2e9ba837b2f6cf3b16e1dc2cf4
kibetamos <amos.rotich@strathmore.edu>
2023-09-08 12:36:18 +0300

    added news to the views

--- home/views.py
+++ home/views.py
@@ -6,3 +6,14 @@ def credits(request):
     content = "Amok"
 
     return HttpResponse(content, content_type="text/plain")
+
+
+def news(request):
+    data={
+        'news':[
+            "RiffMates now has a news page!",
+            "RiffMates has its first web page",
+        ],
+    }
+
+    return render(request, "news.html", data)
