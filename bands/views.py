0b261dfe8886794077af8441c28ea0992fd9b623
kibetamos <amos.rotich@strathmore.edu>
2023-09-09 22:29:31 +0300

    added views to the app

--- bands/views.py
+++ bands/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+# RiffMates/bands/views.py
+from django.shortcuts import render, get_object_or_404 #1
+
+from bands.models import Musician #2
+
+def musician(request, musician_id):
+    musician = get_object_or_404(Musician, id=musician_id) #3
+    
+    data = {
+        "musician": musician, #4
+            }
+    
+    return render(request, "musician.html", data)
