559ca328f45435ec9438e397b0659917782a4b62
kibetamos <amos.rotich@strathmore.edu>
2023-09-11 18:36:15 +0300

    updated utrls

--- bands/urls.py
+++ bands/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
 from bands import views
+# from django.urls import path
 
-urlpattern = [
+urlpatterns = [
 
     path('musician/<int:musician_id>/', views.musician, name="music")
 ]
+
+# urlpatterns = [
+# path('musician/<int:musician_id>/', views.musician, name="musician"),
+# ]
