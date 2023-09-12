8a1454fc636be020bd2615c27d6586153e4eb138
kibetamos <amos.rotich@strathmore.edu>
2023-09-12 18:07:30 +0300

    added bands to the view

--- bands/views.py
+++ bands/views.py
@@ -49,6 +49,26 @@ def band(request, band_id):
     return render(request, "band.html", data)
 
 
+def bands(request):
+    all_bands = Band.objects.all().order_by('name')
+    paginator = Paginator(all_bands, 2)
+
+    page_num = request.GET.get('page', 1)
+    page_num = int(page_num)
+
+    if page_num < 1:
+        page_num =1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+
+    data =  {
+        'bands':page.object_list,
+        'page':page
+    }
+
+    return render(request,'bands.html',data)
 
     # data = {
     #     'musicians':Musician.objects.all().order_by('last_name'),
