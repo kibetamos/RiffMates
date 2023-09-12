6a1a224f222ccac30b24c4ddfaf8f2a6cb53396c
kibetamos <amos.rotich@strathmore.edu>
2023-09-12 07:38:18 +0300

    added bands to the models

--- bands/models.py
+++ bands/models.py
@@ -14,11 +14,17 @@ class Room(models.Model):
 
     def __str__(self):
         return f"Room(id={self.id}, name={self.name})"
-    
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth = models.DateField()
+
+class Band(models.Model):
+    name = models.CharField(max_length=20) 
+    
+    musicians = models.ManyToManyField(Musician)
+    def __str__(self):
+        return f"Band(id={self.id}, name={self.name})"
 
 
