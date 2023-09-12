70051f6ef4575adfeb1bcc768edbd05048794432
kibetamos <amos.rotich@strathmore.edu>
2023-09-12 07:27:34 +0300

    added venue to the models

--- bands/models.py
+++ bands/models.py
@@ -8,6 +8,14 @@ class Venue(models.Models):
     def __str__(self):
         return f"Venue(id={self.id}, name={self.name})"
 
+class Room(models.Model):
+    name = models.CharField(max_length=20)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Room(id={self.id}, name={self.name})"
+    
+    
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
