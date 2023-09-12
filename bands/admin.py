65c6b915b3c658e6aeaf364ade1bb778c5cbd265
kibetamos <amos.rotich@strathmore.edu>
2023-09-12 19:39:06 +0300

    updated admin

--- bands/admin.py
+++ bands/admin.py
@@ -7,6 +7,7 @@ from bands.models import Musician, Venue, Room
 @admin.register(Musician)
 class MusicianAdmin(admin.ModelAdmin):
     list_display = ('id', 'last_name', 'show_weekday')
+    search_fields = ("last_name", "first_name", )
 
     def show_weekday(self, obj):
         # Fetch weekday of artist's birth
