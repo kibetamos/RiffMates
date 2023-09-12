ec4aea0355339d072ec86e7ae5c2900909097fd2
kibetamos <amos.rotich@strathmore.edu>
2023-09-12 17:44:36 +0300

    added urls

--- bands/urls.py
+++ bands/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     path('musician/<int:musician_id>/', views.musician, name="music"),
     path('musicians/', views.musicians, name="musicians"),
+    path('band/<int:band_id>/', views.band, name="band"),
+
 ]
 
 # urlpatterns = [
