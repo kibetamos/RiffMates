9cb109314ea81a335076f75f31fc51683753796a
kibetamos <amos.rotich@strathmore.edu>
2023-09-12 18:55:23 +0300

    updated

--- bands/urls.py
+++ bands/urls.py
@@ -5,8 +5,10 @@ from bands import views
 
 urlpatterns = [
 
-    path('musician/<int:musician_id>/', views.musician, name="music"),
+    path('musician/<int:musician_id>/', views.musician, name="musician"),
+
     path('musicians/', views.musicians, name="musicians"),
+
     path('band/<int:band_id>/', views.band, name="band"),
     path('bands/', views.bands, name="bands"),
     path('venues/', views.venues, name='venue_list'),
