16fdf271627aa3b5c2d9da402feaa6981a6f3e0b
kibetamos <amos.rotich@strathmore.edu>
2023-09-13 16:27:38 +0300

    added filter functionality

--- bands/admin.py
+++ bands/admin.py
@@ -37,7 +37,7 @@ class DecadeListFilter(admin.SimpleListFilter):
 
 @admin.register(Musician)
 class MusicianAdmin(admin.ModelAdmin):
-    list_display = ('id', 'last_name', 'show_weekday')
+    list_display = ('id', 'last_name', 'first_name', 'birth', 'show_weekday')
     search_fields = ("last_name", "first_name", )
 
     def show_weekday(self, obj):
