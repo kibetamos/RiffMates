966a14a5e681735341709a20c891ad19ad48736a
kibetamos <amos.rotich@strathmore.edu>
2023-09-15 13:15:05 +0300

    update urls

--- bands/urls.py
+++ bands/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('musician/<int:musician_id>/', views.musician, name="musician"),
 
-    path('musicians/', views.musicians, name="musicians"),
+    path('', views.musicians, name="musicians"),
 
     path('band/<int:band_id>/', views.band, name="band"),
 
