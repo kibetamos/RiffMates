3c02c1107b7bfb2dab39be4668672eb43edbb31b
kibetamos <amos.rotich@strathmore.edu>
2023-11-20 11:35:49 +0300

    Create add musician

--- bands/forms.py
+++ bands/forms.py
@@ -4,4 +4,4 @@ from .models import Musician  # Assuming you have a Musician model
 class MusicianForm(forms.ModelForm):
     class Meta:
         model = Musician
-        fields = ['first_name', 'last_name', 'other_fields_you_want_to_include']
+        fields = ['first_name', 'last_name', 'birth']
