d057eb73344387eca9f2a4ea5092670bdea8cabe
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-25 23:40:54 +0300

    added home urls

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,11 @@ from .views import add_musician_view
 from bands import views
 # from django.urls import path
 # from .views import add_musician_view, musician_detail, home
+
+
 url_patterns = [
+
+    
 ]
 
 
