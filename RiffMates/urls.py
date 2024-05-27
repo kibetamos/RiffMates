7f91075d9d557aa9f2f9b1d2f8ea507465dcfe77
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-27 09:04:07 +0300

    authentication and authorization

--- RiffMates/urls.py
+++ RiffMates/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from home import views as home_views
 
 urlpatterns = [
+    path('home/', include("home.urls")),
     path('admin/', admin.site.urls),
     path('credits/', home_views.credits, name="credits"),
     path('news/', home_views.news),
--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-from .views import add_musician_view
-from bands import views
-# from django.urls import path
-# from .views import add_musician_view, musician_detail, home
-
+from home import views
+from .views import Register, Login
 
 url_patterns = [
-
-    
+    path('login/', views.Login, name="login"),
+    path('register/', views.Register, name="register"),
 ]
 
 
+
+    # path('', views.musicians, name="musicians"),
+
+    # path('band/<int:band_id>/', views.band, name="band"),
+
+    # path('bands/', views.bands, name="bands"),
+    
+    # path('venues/', views.venues, name='venue_list'),
+
+    # path('add_musician/', add_musician_view, name='add_musician')
