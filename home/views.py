061001f07f050ea0993a5974dcfa909949c8951f
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-27 12:45:50 +0300

    authentication and authorization

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login, logout 
 
 # Create your views here.
 def credits(request):
@@ -38,7 +38,19 @@ def register(request):
 
 def login(request):
     if request.methos == 'POST':
+
         form = LoginForm(request.POST)
+
+        if form.is_valid():
+            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('home')
+    else:
+        form = LoginForm()
 
 
     # content ={ 
