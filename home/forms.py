10da3340fc7f8fbce29b70cfeb4c85e92747ee2a
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-27 12:51:36 +0300

    authentication and authorization

--- home/forms.py
+++ home/forms.py
@@ -1,12 +1,15 @@
-from django import forms 
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignupForm(UserCreationForm):
     class Meta:
-        model = User 
+        model = User
         fields = ['username', 'password1', 'password2']
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+
+
+    class LoginForm(forms.Form):
+        username = forms.CharField()
+        password = forms.CharField
