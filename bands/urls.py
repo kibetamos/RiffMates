99e595d9549dc8b4f0f75fa235d49472cb3b5053
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-31 14:14:13 +0300

    added musicians

--- bands/urls.py
+++ bands/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path('musician/<int:musician_id>/', views.musician, name="musician"),
 
-    path('', views.musicians, name="musicians"),
+    path('musicians/', views.musicians, name="musicians"),
 
     path('band/<int:band_id>/', views.band, name="band"),
 
