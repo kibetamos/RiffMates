66da19af0a25512a9e73f22c89e690dd548b332a
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-06 09:13:26 +0300

    updated the urls views

--- bands/urls.py
+++ bands/urls.py
@@ -8,20 +8,22 @@ urlpatterns = [
 
     path('', home, name='home'),
 
+    path('musicians/', views.musicians, name="musicians"),
+
+    path('add_musician/', add_musician_view, name='add_musician'),
+
     path('musician_detail/<int:id>/', musician_detail, name='musician_detail'),
 
     path('musician/<int:musician_id>/', views.musician, name="musician"),
 
-    path('musicians/', views.musicians, name="musicians"),
 
     path('band/<int:band_id>/', views.band, name="band"),
 
     path('bands/', views.bands, name="bands"),
     
-    path('venues/', views.venues, name='venue_list'),
+    path('create_band/', create_band, name='create_band'),
 
-    path('add_musician/', add_musician_view, name='add_musician'),
 
-    path('create_band/', create_band, name='create_band')
+    path('venues/', views.venues, name='venue_list')
 
 ]
