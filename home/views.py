89cb8514c78e259004569ef37c6737c5004496d8
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-06 11:54:02 +0300

    added authenticate and authorize

--- home/views.py
+++ home/views.py
@@ -2,8 +2,11 @@ from importlib.resources import contents
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth import authenticate, login as auth_login
 from .forms import SignupForm, LoginForm
 from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+
 
 # Create your views here.
 def credits(request):
@@ -42,22 +45,31 @@ def register(request):
 
 
 def login(request):
+    
     if request.method == 'POST':
 
-        form = LoginForm(request.POST)
+        username = request.POST.get('username', '')
 
-        if form.is_valid():
-            
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+        password = request.POST.get('password', '')
+
+        if username and password:
+
             user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)    
-                return redirect('index')
-    else:
-        form = LoginForm()
 
-    return render (request, 'login.html', {'form': form})
+            if user is not None:
+                
+                auth_login(request, user)
+
+                return redirect('home')  # Redirect to the home page or another page
+            else:
+
+                messages.error(request, 'Invalid username or password')
+
+        else:
+
+            messages.error(request, 'Both username and password are required')
+
+    return render(request, 'login.html')
 
 
 # logout page
