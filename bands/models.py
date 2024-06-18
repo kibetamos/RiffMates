3658a4ab1aca7a9991279f0541d1545363c7263c
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-18 12:11:32 +0300

    added performance model

--- bands/models.py
+++ bands/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
-
 from django.core.validators import MinValueValidator
 # Create your models here.]
+
+
 
 class Venue(models.Model):
     name = models.CharField(max_length=20)
@@ -29,7 +29,7 @@ class Room(models.Model):
 
 class Band(models.Model):
     name = models.CharField(max_length=100)
-    genre = models.CharField(max_length=50``)
+    genre = models.CharField(max_length=50)
     
     # def __str__(self):
     #     return f"Band(id={self.id}, name={self.name})"
